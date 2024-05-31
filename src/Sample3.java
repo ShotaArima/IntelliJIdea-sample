bdb2e4ff1aebf71e0feec99ee149e3256418be22
ShotaArima <g2172010@tcu.ac.jp>
2024-06-01 01:10:47 +0900

    upload Sample3

---
+++ src/Sample3.java
@@ -0,0 +1,9 @@
+class Sample3 {
+    public static void main(String[] args) {
+        /* 一文字の場合'' */
+        System.out.println('A');
+        /* 文字列の場合"" */
+        System.out.println("ようこそJAVAへ!");
+        System.out.println(123);
+    }
+}
