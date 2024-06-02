aa1df53df69bccd3d22504cf2d14892e13a9d57c
ShotaArima <g2172010@tcu.ac.jp>
2024-06-02 11:22:48 +0900

    upload Repo2A

---
+++ src/Repo/Repo2A.java
@@ -0,0 +1,10 @@
+class Repo2A {
+    public static void main(String[] args) {
+        System.out.println("Programming Community Index");
+        System.out.println("Lang.\t2021\t2016\t2011\t2006");
+        System.out.println("C\t\t1\t2\t2\t2");
+        System.out.println("Python\t2\t5\t6\t8");
+        System.out.println("Java\t3\t1\t1\t1");
+        System.out.println("C++\t\tp4\tp3\tp3\tp3");
+    }
+}
