09059db39cc59e75f6e4e485dd916c7b28dccbcb
ShotaArima <g2172010@tcu.ac.jp>
2024-06-20 19:21:59 +0900

    upload sample9

---
+++ src/books/Sample9.java
@@ -0,0 +1,16 @@
+package books;
+
+public class Sample9 {
+    public static void main(String[] args) {
+        int num1 = 10;
+        int num2 = 5;
+
+        System.out.println("num1とnum2に色々演算を行います。");
+        System.out.println("num1="+num1+"\nnum2="+num2);
+        System.out.println("num1+num2="+(num1+num2));
+        System.out.println("num1-num2="+(num1-num2));
+        System.out.println("num1*num2="+(num1*num2));
+        System.out.println("num1%num2="+(num1%num2));
+        System.out.println("num1/num2="+(num1/num2));
+    }
+}
