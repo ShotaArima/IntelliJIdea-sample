95043971a37069a33d956ee48549d87d46a6d000
ShotaArima <g2172010@tcu.ac.jp>
2024-08-20 00:01:24 +0900

    add return method

--- src/books/Sample17.java
+++ src/books/Sample17.java
@@ -12,6 +12,14 @@ class Car{
         System.out.println("これから車の情報を表示します");
         this.show();
     }
+    void setBum(int n){
+        this.num = n;
+        System.out.println("車のナンバーを"+this.num+"にしました");
+    }
+    int getNum(){
+        System.out.println("ナンバーを調べました");
+        return this.num;
+    }
 }
 
 public class Sample17 {
@@ -23,6 +31,8 @@ public class Sample17 {
 //        System.out.println("車のナンバーは"+car1.num+"です。");
 //        System.out.println("ガソリンの量は"+car1.gas+"です。");
         car1.showCar(); // メソッドを使用することで簡単に出力することができる
-
+        car1.setBum(5);
+        car1.showCar();
+        System.out.println("ナンバーは"+car1.getNum()+"です。");
     }
 }
