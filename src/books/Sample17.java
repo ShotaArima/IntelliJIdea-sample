9332f86ed4a1a26c547448a7a136f8451414ce90
ShotaArima <g2172010@tcu.ac.jp>
2024-09-01 02:50:56 +0900

    オブジェクトの配列構造に再設計

--- src/books/Sample17.java
+++ src/books/Sample17.java
@@ -3,12 +3,10 @@ package books;
 class Car {
     private int num;
     private double gas;
-    private String name;
 
     public Car() {
         num = 0;
         gas = 0.0;
-        name = "名無し";
         System.out.println("車を作成しました。");
     }
     public void setCar(int n, double g) {
@@ -16,43 +14,26 @@ class Car {
         gas = g;
         System.out.println("ナンバーを"+num+"にガソリン量を"+gas+"にしました");
     }
-    public void setName(String nm) {
-        name = nm;
-        System.out.println("名前を"+name+"にしました。");
-    }
     public void show() {
         System.out.println("車のナンバーは"+num+"です");
         System.out.println("ガソリン量は"+gas+"です");
-        System.out.println("名前は"+name+"です。");
     }
 }
 
 class Sample17 {
     public static void main(String[] args) {
-        Car car1;
-        System.out.println("car1を宣言しました");
-        car1 = new Car(); // 1つのオブジェクトを作成してcar1に代入しました
-        car1.setCar(1234, 20.5);
-        car1.setName("1号車");
-        car1.show();
+        Car[] cars;
+        cars = new Car[3];
 
-        Car car2;
-        System.out.println("car2を宣言しました");
+        for (int i=0; i<cars.length; i++) {
+            cars[i] = new Car();
+        }
+        cars[0].setCar(1234, 20.5);
+        cars[1].setCar(2345, 30.5);
+        cars[2].setCar(3456, 40.5);
 
-        car2 = car1;
-        System.out.println("car2にcar1を代入しました");
-
-        System.out.println("car1がさす");
-        car1.show();
-        System.out.println("car2がさす");
-        car2.show();
-
-        System.out.println("car1がさす車に変更を加えます。");
-        car1.setCar(2345, 20.5);
-
-        System.out.println("car1がさす");
-        car1.show();
-        System.out.println("car2がさす");
-        car2.show();
+        for (int i=0; i<cars.length; i++) {
+            cars[i].show();
+        }
     }
 }
