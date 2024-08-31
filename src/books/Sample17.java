ba61b5dfa4d6a1ac0885273660e2f64d5f07e225
ShotaArima <g2172010@tcu.ac.jp>
2024-08-31 19:55:01 +0900

    update sample17, 21, 22

--- src/books/Sample17.java
+++ src/books/Sample17.java
@@ -1,41 +1,49 @@
 package books;
 
 class Car {
-    public static int sum = 0;
-
     private int num;
     private double gas;
 
     public Car() {
         num = 0;
         gas = 0.0;
-        sum++; //コンストラクタが呼び出された時にクラス変数sumの値を1つ増やす
-        System.out.println("車を作成しました");
+        System.out.println("車を作成しました。");
     }
     public void setCar(int n, double g) {
         num = n;
         gas = g;
-        System.out.println("ナンバーを"+num+"にガソリン量を"+gas+"にしました。");
-    }
-    public static void showSum() {
-        System.out.println("車は全部で"+sum+"台です。");
+        System.out.println("ナンバーを"+num+"にガソリン量を"+gas+"にしました");
     }
     public void show() {
-        System.out.println("車のナンバーは"+num+"です。");
-        System.out.println("ガソリン量は"+gas+"です。");
+        System.out.println("車のナンバーは"+num+"です");
+        System.out.println("ガソリン量は"+gas+"です");
     }
 }
 
 class Sample17 {
     public static void main(String[] args) {
-        Car.showSum();
-        Car car1 = new Car();
+        Car car1;
+        System.out.println("car1を宣言しました");
+        car1 = new Car(); // 1つのオブジェクトを作成してcar1に代入しました
         car1.setCar(1234, 20.5);
-        Car.showSum();
 
-        Car car2 = new Car();
-        car2.setCar(4567, 30.5);
+        Car car2;
+        System.out.println("car2を宣言しました");
 
-        Car.showSum();
+        car2 = car1;
+        System.out.println("car2にcar1を代入しました");
+
+        System.out.println("car1がさす");
+        car1.show();
+        System.out.println("car2がさす");
+        car2.show();
+
+        System.out.println("car1がさす車に変更を加えます。");
+        car1.setCar(2345, 20.5);
+
+        System.out.println("car1がさす");
+        car1.show();
+        System.out.println("car2がさす");
+        car2.show();
     }
 }
