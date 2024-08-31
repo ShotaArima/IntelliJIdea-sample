77f096df03fc6ed82e07c67f29a7c6b04caa0a14
ShotaArima <g2172010@tcu.ac.jp>
2024-09-01 00:57:10 +0900

    add name

--- src/books/Sample17.java
+++ src/books/Sample17.java
@@ -3,10 +3,12 @@ package books;
 class Car {
     private int num;
     private double gas;
+    private String name;
 
     public Car() {
         num = 0;
         gas = 0.0;
+        name = "名無し";
         System.out.println("車を作成しました。");
     }
     public void setCar(int n, double g) {
@@ -14,9 +16,14 @@ class Car {
         gas = g;
         System.out.println("ナンバーを"+num+"にガソリン量を"+gas+"にしました");
     }
+    public void setName(String nm) {
+        name = nm;
+        System.out.println("名前を"+name+"にしました。");
+    }
     public void show() {
         System.out.println("車のナンバーは"+num+"です");
         System.out.println("ガソリン量は"+gas+"です");
+        System.out.println("名前は"+name+"です。");
     }
 }
 
@@ -26,6 +33,8 @@ class Sample17 {
         System.out.println("car1を宣言しました");
         car1 = new Car(); // 1つのオブジェクトを作成してcar1に代入しました
         car1.setCar(1234, 20.5);
+        car1.setName("1号車");
+        car1.show();
 
         Car car2;
         System.out.println("car2を宣言しました");
