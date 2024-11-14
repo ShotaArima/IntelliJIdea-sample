d2279ec4130ded3c355f1762a0c6fe8e5168c5d8
ShotaArima <g2172010@tcu.ac.jp>
2024-11-14 19:06:18 +0900

    update Sample2,3

--- src/books/Sample3.java
+++
@@ -1,9 +0,0 @@
-class Sample3 {
-    public static void main(String[] args) {
-        /* 一文字の場合'' */
-        System.out.println('A');
-        /* 文字列の場合"" */
-        System.out.println("ようこそJAVAへ!");
-        System.out.println(123);
-    }
-}
