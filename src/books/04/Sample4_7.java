6740a10107a6be1885cda187e56bebb47d5c421b
ShotaArima <g2172010@tcu.ac.jp>
2024-11-15 18:09:35 +0900

    refactoring sample-code4_7 P95

---
+++ src/books/04/Sample4_7.java
@@ -0,0 +1,6 @@
+class Sample4_7 {
+    public static void main(String[] args) {
+        System.out.println("1+2は"+1+2+"です。"); // 括弧をつけない
+        System.out.println("3+4は"+3+4+"です。"); // 括弧をつけない
+    }
+}
